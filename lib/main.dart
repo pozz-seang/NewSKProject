7dab8b87ef636b092d0aa33b14552175add5d765
SEHacker <69587550+pozz-seang@users.noreply.github.com>
2022-08-20 21:58:40 +0700

    h

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
+import 'package:newskproject/spinwheelscreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,48 +11,61 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
+    FlutterNativeSplash.remove();
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: SpinWheel(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
 
-  final String title;
+class SpinningWheel extends StatefulWidget {
+  const SpinningWheel({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<SpinningWheel> createState() => _SpinningWheelState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _SpinningWheelState extends State<SpinningWheel> {
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-          child: Column(
-        children: <Widget>[
-          TextButton(
-            child: Text("Enable privacy screen"),
-            onPressed: () async {},
+    return Container(
+      color: Color.fromARGB(255, 255, 0, 0),
+      child: Column(children: [
+        Flexible(
+          flex: 1,
+          child: Container(
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 111,
+                  left: 45,
+                  child: Image(
+                    image: AssetImage("assets/roulette-8-300.png"),
+                    height: 270,
+                  ),
+                ),
+                Positioned(
+                  top: 210,
+                  left: 145,
+                  child: Image(
+                    image: AssetImage("assets/roulette-center-300.png"),
+                    height: 70,
+                  ),
+                ),
+              ],
+            ),
           ),
-          TextButton(
-            child: Text("Disable privacy screen"),
-            onPressed: () async {},
-          )
-        ],
-      )),
+        ),
+      ]),
     );
   }
 }
